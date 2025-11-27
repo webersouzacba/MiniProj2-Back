c2d57ef79da560f83579eb7331ad8831631a69cf
Weber Souza <45805173+webersouzacba@users.noreply.github.com>
2025-11-27 11:36:01 -0400

    Ajustes na organização das pastas do projeto

--- Start_Animalec.bat
+++ Start_Animalec.bat
@@ -1,20 +1,16 @@
 @echo off
 title Start Animalec
 
-REM Garante Node 16 para este CMD (por via das dúvidas)
+REM Garante Node 16
 call nvm use 16.20.2
 
-REM Pasta base do projeto Animalec (ajuste se o caminho for diferente)
-set "BASE_DIR=E:\MEIW\2025_2026_1\Programação Web Avançada 2025\Animalec_livro\Animalec"
+REM Caminho da pasta onde o .bat está
+set "BASE=%~dp0"
 
-REM ---- Janela 1: BACK-END ----
+REM ---- BACK-END ----
 start "Animalec Backend" cmd /k ^
-"nvm use 16.20.2 ^& ^
-cd /d \"%BASE_DIR%\Back-end_Animalec-master\" ^& ^
-npm start"
+"cd /d \"%BASE%Back-end_Animalec-master\" ^&^& nvm use 16.20.2 ^&^& npm start"
 
-REM ---- Janela 2: FRONT-END ----
+REM ---- FRONT-END ----
 start "Animalec Frontend" cmd /k ^
-"nvm use 16.20.2 ^& ^
-cd /d \"%BASE_DIR%\Front-end_Animalec-master\" ^& ^
-npm run serve"
+"cd /d \"%BASE%Front-end_Animalec-master\" ^&^& nvm use 16.20.2 ^&^& npm run serve"
